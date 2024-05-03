b6946e304bb39873c74965d4a523b2353911d4e9
Anastasiia <humenyuknastia.9616@gmail.com>
2024-05-03 12:20:41 +0200

    componets for displaying Meal

---
+++ meal-sharing-project/src/components/MealDetail.jsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
+import MealHeader from "./MealHeader";
+import ReservationSection from "./reservation/ReservationSection";
+import ReviewForm from "./Review/ReviewForm";
+
+const MealDetail = () => {
+  const { id } = useParams();
+  const apiUrl = `http://127.0.0.1:5001/api/meals/${id}`;
+  const [meal, setMeal] = useState(null);
+  const { data: mealData, isLoading, error } = useFetch(apiUrl);
+
+  useEffect(() => {
+    if (mealData && mealData.meal) {
+      setMeal(mealData.meal[0]);
+    }
+  }, [mealData]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!meal) {
+    return <div>Meal not found</div>;
+  }
+
+  return (
+    <div className="MealDetail-container">
+      <MealHeader meal={meal} />
+      <div className="MealDetail-button-container">
+        <ReservationSection meal={meal} mealId={id} />
+        <ReviewForm mealId={id} />
+      </div>
+    </div>
+  );
+};
+
+export default MealDetail;
