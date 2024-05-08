cde75c50d60713ca327eab3d8a5d4495e222f31b
Anastasiia <humenyuknastia.9616@gmail.com>
2024-05-08 12:55:01 +0200

    added footer

---
+++ meal-sharing-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Footer.css"; // Import the CSS file
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-column">
+        <h3>About Us</h3>
+        <ul>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/contact">Contact Us</a>
+          </li>
+          <li>
+            <a href="/privacy">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="/terms">Terms of Service</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h3>Contact Us</h3>
+        <div className="footer-contact-info">
+          <p>
+            Email: <a href="mailto:example@example.com">example@example.com</a>
+          </p>
+          <p>
+            Phone: <a href="tel:123-456-7890">123-456-7890</a>
+          </p>
+          <p>Address: 123 Main St, City, Country</p>
+        </div>
+      </div>
+      <div className="footer-column">
+        <h3>Social Media</h3>
+        <div className="footer-social-icons">
+          <a href="https://www.facebook.com">
+            <img src="/streamline-logos/Facebook-Logo.svg" alt="Facebook" />
+          </a>
+
+          <a href="https://www.instagram.com">
+            <img src="/streamline-logos/Instagram-Logo.svg" alt="Instagram" />
+          </a>
+          <a href="https://www.linkedin.com">
+            <img src="/streamline-logos/Linkedin-Logo.svg" alt="LinkedIn" />
+          </a>
+        </div>
+      </div>
+      <p className="footer-rights">&copy; 2024. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
