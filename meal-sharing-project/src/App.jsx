bcc3156cf1877f46cdbd88bcbdb28472fdeeb603
Anastasiia <humenyuknastia.9616@gmail.com>
2024-05-09 17:39:13 +0200

    updait router

--- meal-sharing-project/src/App.jsx
+++ meal-sharing-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MealsList from "./components/MealsList";
 import MealDetail from "./components/MealDetail";
